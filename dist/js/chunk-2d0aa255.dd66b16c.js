79aafc572fead65a64ef99d7499274c47dcb5bbb
zhou886 <1272686158@qq.com>
2022-01-16 19:52:06 +0800

    add bulid part

---
+++ dist/js/chunk-2d0aa255.dd66b16c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aa255"],{1088:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"record"},[n._v(" 种树记录界面 ")])},c=[],s=t("2877"),l={},r=Object(s["a"])(l,a,c,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0aa255.dd66b16c.js.map
---
+++ dist/precache-manifest.ca09f82a1d4a87fa61d9967e7a6526be.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129c090c4fbc882146ec",
+    "url": "/css/app.9c4d7508.css"
+  },
+  {
+    "revision": "f2f202fb597b1ebe9e4a",
+    "url": "/css/chunk-0c11af76.67fab4db.css"
+  },
+  {
+    "revision": "84d631061a90f8e29d54",
+    "url": "/css/chunk-16609646.4c580f25.css"
+  },
+  {
+    "revision": "19b9a0d3dfa85e3fed8b",
+    "url": "/css/chunk-6eb35bbf.d24da278.css"
+  },
+  {
+    "revision": "90ca724dcdd21a6ece6c",
+    "url": "/css/chunk-8b2132b2.777995b5.css"
+  },
+  {
+    "revision": "3e7bf13d8b4b6c2ee138",
+    "url": "/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "26b3e896a303514aab3a435c1cd09542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "129c090c4fbc882146ec",
+    "url": "/js/app.d8cace6f.js"
+  },
+  {
+    "revision": "f2f202fb597b1ebe9e4a",
+    "url": "/js/chunk-0c11af76.7e4b3fd1.js"
+  },
+  {
+    "revision": "84d631061a90f8e29d54",
+    "url": "/js/chunk-16609646.baf7fde8.js"
+  },
+  {
+    "revision": "ea14647a1fe3776166e1",
+    "url": "/js/chunk-2d0aa255.dd66b16c.js"
+  },
+  {
+    "revision": "19b9a0d3dfa85e3fed8b",
+    "url": "/js/chunk-6eb35bbf.ec8853f9.js"
+  },
+  {
+    "revision": "ee633225c8669b5970f4",
+    "url": "/js/chunk-705f53be.107cb3a7.js"
+  },
+  {
+    "revision": "90ca724dcdd21a6ece6c",
+    "url": "/js/chunk-8b2132b2.355e7c77.js"
+  },
+  {
+    "revision": "3e7bf13d8b4b6c2ee138",
+    "url": "/js/chunk-vendors.1b8fc29c.js"
+  },
+  {
+    "revision": "73bab5d8ad0d3bccbfdd7433d554bd5a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
